94da9d7bd686554f9b5ee4b5dcdf2276883fd83d
sandilem7789 <sandilem7789@gmail.com>
2021-08-31 15:38:20 +0200

    modal on screen

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuSharp from '@material-ui/icons/MenuSharp';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuSharp from "@material-ui/icons/MenuSharp";
 import Grid from "@material-ui/core/Grid";
 
 /* Modal */
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
 /* CUSTOM COMPONENTS */
-import Videos from './Categories/Videos';
-import Blog from './Categories/Blog';
-import Podcasts from './Categories/Podcasts';
-import WelcomeText from './WelcomeText';
+import Videos from "./Categories/Videos";
+import Blog from "./Categories/Blog";
+import Podcasts from "./Categories/Podcasts";
+import WelcomeText from "./WelcomeText";
 
 const drawerWidth = 0;
-const backgroundColor= "#100c08"
-const fontColor = "#fff"
+const backgroundColor = "#100c08";
+const fontColor = "#fff";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    backgroundColor: backgroundColor
+    display: "flex",
+    backgroundColor: backgroundColor,
   },
   appBar: {
     backgroundColor: backgroundColor,
     color: fontColor,
     boxShadow: "0px 0px 0px #36454F",
-    height: "10px"
+    height: "10px",
   },
   appBarShift: {
     width: `100%`,
@@ -44,10 +44,10 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     fontFamily: "Rampart One, sans-serif",
     marginBottom: "10px",
-    height: "62px"
+    height: "62px",
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -57,12 +57,12 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
   },
   content: {
     flexGrow: 1,
@@ -70,7 +70,7 @@ const useStyles = makeStyles((theme) => ({
     marginRight: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -78,13 +78,13 @@ const useStyles = makeStyles((theme) => ({
   },
   //to be moved
   modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -107,19 +107,19 @@ export default function LandingPage() {
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
-        position="fixed"
+        position='fixed'
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
       >
         <Toolbar>
-          <Typography variant="h3" noWrap className={classes.title}>
+          <Typography variant='h3' noWrap className={classes.title}>
             IsiZulu.js
           </Typography>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
+            color='inherit'
+            aria-label='open drawer'
+            edge='end'
             onClick={handleOpen}
             className={clsx(open && classes.hide)}
           >
@@ -134,52 +134,50 @@ export default function LandingPage() {
       >
         <div className={classes.drawerHeader} />
         <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <div>
-                    <WelcomeText />
-                </div>
-            </Grid>
-            <Grid item xs={12} md={12}>
-                <a href="http://localhost:3000/">
-                    <Videos />
-                </a>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <a href="http://localhost:3000/">
-                    <Blog />
-                </a>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <a href="http://localhost:3000/">
-                    <Podcasts />
-                </a>
-            </Grid>
+          <Grid item xs={12}>
+            <div>
+              <WelcomeText />
+            </div>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <a href='http://localhost:3000/'>
+              <Videos />
+            </a>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <a href='http://localhost:3000/'>
+              <Blog />
+            </a>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <a href='http://localhost:3000/'>
+              <Podcasts />
+            </a>
+          </Grid>
         </Grid>
         <div>
-      <button type="button" onClick={handleOpen}>
-        react-transition-group
-      </button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
-          </div>
-        </Fade>
-      </Modal>
-    </div>
- 
+          <Modal
+            aria-labelledby='transition-modal-title'
+            aria-describedby='transition-modal-description'
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <h2 id='transition-modal-title'>Transition modal</h2>
+                <p id='transition-modal-description'>
+                  react-transition-group animates me.
+                </p>
+              </div>
+            </Fade>
+          </Modal>
+        </div>
       </main>
     </div>
   );
--- src/components/OptionsModal.js
+++ src/components/OptionsModal.js
@@ -1,59 +1,36 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import React from 'react'
 
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function TransitionsModal() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <button type="button" onClick={handleOpen}>
-        react-transition-group
-      </button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
-          </div>
-        </Fade>
-      </Modal>
-    </div>
-  );
+const OptionsModal = ({open, handleClose}) => {
+    return (
+        <div>
+            <Modal
+            aria-labelledby='transition-modal-title'
+            aria-describedby='transition-modal-description'
+            className="modal-paper"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+            >
+            <Fade in={open}>
+              <div className="modal-paper">
+                <h2 id='transition-modal-title'>Transition modal</h2>
+                <p id='transition-modal-description'>
+                  Imported Modal
+                </p>
+              </div>
+            </Fade>
+          </Modal>
+        </div>
+    )
 }
+
+export default OptionsModal
